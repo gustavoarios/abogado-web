69c077f1682e9c8b7b41be5512861af35809fe21
Gustavo Ríos <gustavoadonairios@gmail.com>
2025-09-13 19:49:41 -0300

    Versión inicial de la web de abogado

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("form-contacto").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("¡Tu consulta fue enviada! (acá iría la integración con EmailJS)");
+});
