326c68af79f660ebec5aab1ee0b443ce9b0405e6
gustavoarios <gustavoadonairios@gmail.com>
2025-09-15 23:18:15 -0300

    servicios, etc

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ document.getElementById("form-contacto").addEventListener("submit", function(e) 
   e.preventDefault();
   alert("¡Tu consulta fue enviada! (acá iría la integración con EmailJS)");
 });
+
+<script>
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault(); // evita el salto instantáneo
+    const target = document.querySelector(link.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+</script>
